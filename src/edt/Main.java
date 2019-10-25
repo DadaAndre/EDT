0b2622c51ac4292ed13f4b39d502279b50f90742
MartinJ <martinj50220@gmail.com>
2019-10-25 21:03:20 +0200

    feat : Ajout de UnitTest
    Classe utilitaire qui permet de formaliser de manière claire le résultat des différents tests à réaliser sur le programme

--- src/edt/Main.java
+++ src/edt/Main.java
@@ -2,24 +2,35 @@ package edt;
 
 import edt.activity.*;
 import edt.constraints.*;
+import edt.tests.UnitTest;
 
 public class Main {
-  public static void main(String[] args){
+	public static void main(String[] args){
 
-    // Objets
-    Activity options = new Activity ("Choisir mes options", 70);
-    Activity ip = new Activity ("Inscription pédagogique", 30);
-    PrecedenceConstraint contrainte = new PrecedenceConstraint (options, ip);
-    PrecedenceConstraintWithGap contrainte2 = new PrecedenceConstraintWithGap(options, ip, 30);
+		// Objets
+		Activity options = new Activity ("Choisir mes options", 70);
+		Activity ip = new Activity ("Inscription pédagogique", 30);
+		PrecedenceConstraint contrainte = new PrecedenceConstraint (options, ip);
+		PrecedenceConstraintWithGap contrainte2 = new PrecedenceConstraintWithGap(options, ip, 30);
 
-    int neufHeure = 9;
-    int dixHeure = 10;
-    int onzeHeure = 11;
-    int douzeHeure = 12;
+		int neufHeure = 9;
+		int dixHeure = 10;
+		int onzeHeure = 11;
+		int douzeHeure = 12;
 
-    Test testSansGap = new Test(contrainte,neufHeure,dixHeure,onzeHeure);
-    Test testAvecGap = new Test(contrainte2,neufHeure,dixHeure,douzeHeure);
+		//Test testSansGap = new Test(contrainte,neufHeure,dixHeure,onzeHeure);
+		//Test testAvecGap = new Test(contrainte2,neufHeure,dixHeure,douzeHeure);
 
-  }
+		UnitTest.setTestLabel("PrecedenceConstraintWithGap");
+		UnitTest.isTrue(true);
+		UnitTest.isTrue(true);
+		UnitTest.isTrue(false);
+		UnitTest.isTrue(true);
+		UnitTest.setTestLabel("PrecedenceConstraint");
+		UnitTest.isTrue(false);
+		UnitTest.isTrue(true);
+		UnitTest.isFalse(true);
+		UnitTest.summary();
+	}
 
 }
