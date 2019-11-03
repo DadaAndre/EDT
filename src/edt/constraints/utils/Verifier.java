c7373d25d2cf357c9a5a30d546ac8f12f20fef56
Dada Andre <dada.andre2@gmail.com>
2019-11-03 21:11:10 +0100

    doc : Javadoc pour la classe Verifier

--- src/edt/constraints/utils/Verifier.java
+++ src/edt/constraints/utils/Verifier.java
@@ -6,22 +6,41 @@ import edt.activity.Activity;
 import edt.constraints.*;
 import java.util.GregorianCalendar;
 
+/**
+* Permet de verifier qu'un ensemble de contraintes est satisfait par un emploi du temps
+*/
 public class Verifier {
 
+	/**
+	* Liste des contraintes à verifier
+	*/
 	private ArrayList<Constraint> listOfConstraint;
+
 
 	public Verifier() {
 		this.listOfConstraint = new ArrayList<>(); //Création d'une ArrayList contenant la liste des contraintes
 	}
 
-	public void addConstraint(Constraint constraint) { //ajout d'une contrainte
+
+	/**
+	* Permet d'ajouter une contrainte à verifier
+	*
+	* @param constraint La contrainte à ajouter
+	*/
+	public void addConstraint(Constraint constraint) {
 		this.listOfConstraint.add(constraint);
 	}
 
+
+	/**
+	* Verifie que chaque contraintes est satisfaite par l'emploi du temps fourni
+	*
+	* @param edt L'emploi du temps
+	* @return True si toute les contraintes sont satisfaites, False sinon
+	*/
 	public boolean verify(HashMap<Activity,GregorianCalendar> edt) {
 		for(Constraint c : listOfConstraint) { //on essaye chaque contraintes
 
-			//on regarde que l'emploi du temps satisfait la contrainte actuelle
 			if(c.isSatisfiedBySchedule(edt) == false) { //si une seule condition est fausse, on retourne false
 				return false;
 			}
