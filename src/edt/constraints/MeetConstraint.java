2e06f07dc9c78b4a007c8a2b2a917092f99b75f1
MartinJ <martinj50220@gmail.com>
2019-11-03 17:53:47 +0100

    doc : Javadoc pour la classe MeetConstraint

--- src/edt/constraints/MeetConstraint.java
+++ src/edt/constraints/MeetConstraint.java
@@ -1,15 +1,24 @@
 package edt.constraints;
+
 import edt.activity.Activity;
 import java.util.GregorianCalendar;
 
+/**
+* Contrainte binaire satisfaite lorsque la première activité se termine quand la deuxième commence
+*/
 public class MeetConstraint extends BinaryConstraint{
 
-    public MeetConstraint(Activity first, Activity second){
-        super(first, second);
-    }
+	/**
+	* @param first La première activité
+	* @param second La deuxième activité
+	*/
+	public MeetConstraint(Activity first, Activity second){
+		super(first, second);
+	}
 
-    @Override
-    public boolean isSatisfied(GregorianCalendar dateDebutAct1, GregorianCalendar dateDebutAct2){
+
+	@Override
+	public boolean isSatisfied(GregorianCalendar dateDebutAct1, GregorianCalendar dateDebutAct2){
 		GregorianCalendar dateFinAct1 = new GregorianCalendar(); //création d'un nouveau calendrier.
 		dateFinAct1.setTime(dateDebutAct1.getTime()); // mettre ce nouveau calendrier à la même date que la première activité.
 		dateFinAct1.add(GregorianCalendar.MINUTE, this.getFirstActivity().getDuree()); //on ajoute à l'heure de la première activité, sa durée.
@@ -18,5 +27,5 @@ public class MeetConstraint extends BinaryConstraint{
 		* commence dirrectement après la première activité, alors le compareTo vaut "0".
 		*/
 		return dateFinAct1.compareTo(dateDebutAct2) == 0;
-    }
+	}
 }
