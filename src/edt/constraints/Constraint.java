e009a7ebb42fac517142ba5b339eb304657e6f46
MartinJ <martinj50220@gmail.com>
2019-11-03 14:00:38 +0100

    doc : Javadoc pour l'interface Constraint

--- src/edt/constraints/Constraint.java
+++ src/edt/constraints/Constraint.java
@@ -4,8 +4,17 @@ import java.util.HashMap;
 import edt.activity.Activity;
 import java.util.GregorianCalendar;
 
-public interface Constraint{
-
+/**
+* Interface commune à toutes les contraintes
+*/
+public interface Constraint {
+	
+	/**
+	* Permet de savoir si la constrainte est respectée selon l'emploi du temps fournit en paramètre.
+	*
+	* @param edt L'emploi du temps
+	* @return True si la contrainte est respectée, false sinon
+	*/
   public boolean isSatisfiedBySchedule(HashMap<Activity, GregorianCalendar> edt);
 
 }
