6f92f88e3725046b31d71c282d983e9db4b04678
MartinJ <martinj50220@gmail.com>
2019-11-03 17:49:08 +0100

    doc : Javadoc pour la classe BinaryConstraint

--- src/edt/constraints/BinaryConstraint.java
+++ src/edt/constraints/BinaryConstraint.java
@@ -4,27 +4,63 @@ import edt.activity.Activity;
 import java.util.HashMap;
 import java.util.GregorianCalendar;
 
-public abstract class BinaryConstraint implements Constraint{
+/**
+* Représente une contrainte binaire concernant deux {@link Activity}.
+*/
+public abstract class BinaryConstraint implements Constraint {
 
-    private Activity firstActivity;
-    private Activity secondActivity;
+	/**
+	* La première activité
+	*/
+	private Activity firstActivity;
+	/**
+	* La deuxième activité
+	*/
+	private Activity secondActivity;
 
-    public BinaryConstraint(Activity first, Activity second){
-       this.firstActivity = first;
-       this.secondActivity = second;
-    }
 
-    public abstract boolean isSatisfied(GregorianCalendar dateDebutAct1, GregorianCalendar dateDebutAct2);
+	/**
+	* @param first La première activité
+	* @param second La deuxième activité
+	*/
+	public BinaryConstraint(Activity first, Activity second){
+		this.firstActivity = first;
+		this.secondActivity = second;
+	}
 
-    public boolean isSatisfiedBySchedule(HashMap<Activity, GregorianCalendar> edt){
-       return isSatisfied(edt.get(this.getFirstActivity()),edt.get(this.getSecondActivity()));
-    }
 
-    public Activity getFirstActivity(){
-      return this.firstActivity;
-    }
+	/**
+	* Vérifie si la contrainte est satisfaite, sachant que la première activité commence à dateDebutAct1 et la deuxième commence à dateDebutAct2
+	*
+	* @param dateDebutAct1 Le début de la première activité
+	* @param dateDebutAct1 Le début de la deuxième activité
+	* @return True si la contrainte est satisfaite false sinon
+	*/
+	public abstract boolean isSatisfied(GregorianCalendar dateDebutAct1, GregorianCalendar dateDebutAct2);
 
-    public Activity getSecondActivity(){
-      return this.secondActivity;
-    }
+
+	@Override
+	public boolean isSatisfiedBySchedule(HashMap<Activity, GregorianCalendar> edt){
+		return isSatisfied(edt.get(this.getFirstActivity()),edt.get(this.getSecondActivity()));
+	}
+
+
+	/**
+	* Récupère la première activité
+	*
+	* @return La première activité
+	*/
+	public Activity getFirstActivity(){
+		return this.firstActivity;
+	}
+
+
+	/**
+	* Récupère la seconde activité
+	*
+	* @return La seconde activité
+	*/
+	public Activity getSecondActivity(){
+		return this.secondActivity;
+	}
 }
