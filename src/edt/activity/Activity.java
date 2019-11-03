6713be3d0e308ccab9196cfb837f17f3aa93c08d
MartinJ <martinj50220@gmail.com>
2019-11-03 13:51:01 +0100

    doc : Javadoc pour la classe Activity

--- src/edt/activity/Activity.java
+++ src/edt/activity/Activity.java
@@ -1,19 +1,42 @@
 package edt.activity;
 
+/**
+* Représente des créneaux non positionnés dans le temps
+*/
 public class Activity {
-
+	
+	/**
+	* Description de l'activité
+	*/
 	private String desc;
+	/**
+	* Durée de l'activité
+	*/
 	private int duree;
 
+	/**
+	* @param desc Description de l'activité
+	* @param duree Durée de l'activité
+	*/
 	public Activity(String desc, int duree) {
 		this.desc = desc;
 		this.duree = duree;
 	}
 
+	/**
+	* Récupère la description de l'activité
+	*
+	* @return La description de l'activité
+	*/
 	public String getDesc() {
 		return this.desc;
 	}
 
+	/**
+	* Récupère la durée de l'activité
+	*
+	* @return La durée de l'activité
+	*/
 	public int getDuree() {
 		return this.duree;
 	}
