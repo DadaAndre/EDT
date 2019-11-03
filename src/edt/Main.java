43f2a8610e8a3f23bbc9b2c98200240a4eb9f723
Dada Andre <dada.andre2@gmail.com>
2019-11-03 20:41:11 +0100

    Création de la classe Verifier, création dans la Main d'un Emploi du temps sur une journée, mise à jour des commentaires

--- src/edt/Main.java
+++ src/edt/Main.java
@@ -2,27 +2,52 @@ package edt;
 
 import edt.activity.*;
 import edt.constraints.*;
+import edt.constraints.utils.*;
 import edt.tests.UnitTest;
 import java.util.GregorianCalendar;
+import java.util.HashMap;
 
 public class Main {
 	public static void main(String[] args){
 
-		// Objets
+		//Création des activités
 		Activity options = new Activity ("Choisir mes options", 90);
 		Activity sport = new Activity("Sport", 120);
 		Activity ip = new Activity ("Inscription pédagogique", 30);
+		Activity marche = new Activity("Balade", 75);
+		Activity devoirs = new Activity("Devoirs",60);
 
+		//Création des horaires
 		GregorianCalendar date1_9h = new GregorianCalendar(2019, 9, 15, 9, 0, 0);  //Année/Mois/Jour/Heure/Minute/Seconde
 		GregorianCalendar date1_10h = new GregorianCalendar(2019, 9, 15, 10, 0, 0);
 		GregorianCalendar date1_11h = new GregorianCalendar(2019, 9, 15, 11, 0, 0);
 		GregorianCalendar date1_12h = new GregorianCalendar(2019, 9, 15, 12, 0, 0);
+		GregorianCalendar date1_15h = new GregorianCalendar(2019, 9, 15, 15, 0, 0);
+		GregorianCalendar date1_17h = new GregorianCalendar(2019, 9, 15, 17, 0, 0);
+		GregorianCalendar date1_18h = new GregorianCalendar(2019, 9, 15, 18, 0, 0);
 
+		//initialisation de contraintes
 		PrecedenceConstraint contrainte = new PrecedenceConstraint (sport, ip);
 		PrecedenceConstraintWithGap contrainteWithGap = new PrecedenceConstraintWithGap(options, ip, 60);
 		PrecedenceConstraintWithGap contrainteWithGap2 = new PrecedenceConstraintWithGap(options, ip, 30);
 		MeetConstraint meetConstraint = new MeetConstraint(sport, ip);
+		Verifier verif = new Verifier();
 
+		//Création d'un emploi du temps et ajout des activités avec l'heure à laquelle elles commencent
+		HashMap <Activity, GregorianCalendar> emploiDuTemps = new HashMap<Activity, GregorianCalendar> ();
+		emploiDuTemps.put(sport, date1_9h);
+		emploiDuTemps.put(options, date1_11h);
+		emploiDuTemps.put(marche, date1_15h);
+		emploiDuTemps.put(ip, date1_17h);
+		emploiDuTemps.put(devoirs, date1_18h);
+
+		//On ajoute des contraintes dans la HashMap se trouvant dans la classe Verifier
+		verif.addElement(new MeetConstraint(sport,options));
+		verif.addElement(new PrecedenceConstraintWithGap(options,marche,120));
+		verif.addElement(new PrecedenceConstraint(marche,ip));
+		verif.addElement(new PrecedenceConstraint(ip,devoirs));
+
+		//--------------------------------------- TESTS ---------------------------------------
 		UnitTest.setTestLabel("PrecedenceConstraint");
 		UnitTest.isFalse(contrainte.isSatisfied(date1_11h, date1_9h)); //2nde activité avant la 1ère
 		UnitTest.isFalse(contrainte.isSatisfied(date1_9h, date1_10h)); //2nde activité pendant la 1ère
@@ -40,6 +65,10 @@ public class Main {
 		UnitTest.isFalse(meetConstraint.isSatisfied(date1_9h, date1_10h)); //debut de la 2nde activité trop tôt que la fin de la 1ère
 		UnitTest.isTrue(meetConstraint.isSatisfied(date1_9h, date1_11h)); //debut de la 2nde activité en même temps que la fin de la 1ére
 		UnitTest.isFalse(meetConstraint.isSatisfied(date1_9h, date1_12h)); //debut de la 2nde activité trop tard que la fin de la 1ère
+
+		UnitTest.setTestLabel("Verifier");
+		UnitTest.isTrue(verif.verify(emploiDuTemps)); //emploi du temps conforme avec les contraintes
+
 		UnitTest.summary();
 	}
 
---
+++ src/edt/constraints/utils/Verifier.java
@@ -0,0 +1,31 @@
+package edt.constraints.utils;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import edt.activity.Activity;
+import edt.constraints.*;
+import java.util.GregorianCalendar;
+
+public class Verifier{
+	private ArrayList<Constraint> listOfConstraint;
+
+	public Verifier(){
+		this.listOfConstraint = new ArrayList<>(); //Création d'une ArrayList contenant la liste des contraintes
+	}
+
+	public void addElement(Constraint constraint){ //ajout d'une contrainte
+		this.listOfConstraint.add(constraint);
+	}
+
+	public boolean verify(HashMap<Activity,GregorianCalendar> edt){
+		for(Constraint c : listOfConstraint) { //on essaye chaque contraintes
+
+			//on regarde que l'emploi du temps satisfait la contrainte actuelle
+			if(c.isSatisfiedBySchedule(edt) == false){ //si une seule condition est fausse, on retourne false
+				return false;
+			}
+		} return true;
+	}
+
+
+}
