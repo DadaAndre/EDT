be3e0a7ebc372450e2a60c2e6df3cba228134306
Dada Andre <dada.andre2@gmail.com>
2019-11-03 13:12:12 +0100

    mise a jour des tests ecistants et ajout des tests de meetConstraint. Supression de l'ancienne classe Test

--- src/edt/Main.java
+++ src/edt/Main.java
@@ -8,33 +8,36 @@ public class Main {
 	public static void main(String[] args){
 
 		// Objets
-		Activity options = new Activity ("Choisir mes options", 70);
+		Activity options = new Activity ("Choisir mes options", 90);
+		Activity sport = new Activity("Sport", 120);
 		Activity ip = new Activity ("Inscription pédagogique", 30);
-		PrecedenceConstraint contrainte = new PrecedenceConstraint (options, ip);
-		PrecedenceConstraintWithGap contrainte2 = new PrecedenceConstraintWithGap(options, ip, 30);
 
 		int neufHeure = 9;
 		int dixHeure = 10;
 		int onzeHeure = 11;
 		int douzeHeure = 12;
 
-		//Test testSansGap = new Test(contrainte,neufHeure,dixHeure,onzeHeure);
-		//Test testAvecGap = new Test(contrainte2,neufHeure,dixHeure,douzeHeure);
-
-		MeetConstraint meetConstraint = new MeetConstraint(options2, ip2);
-		System.out.println(meetConstraint.isSatisfied(neufHeure, dixHeure));
-		System.out.println(meetConstraint.isSatisfied(neufHeure, onzeHeure));
-
-		UnitTest.setTestLabel("PrecedenceConstraintWithGap");
-		UnitTest.isTrue(true);
-		UnitTest.isTrue(true);
-		UnitTest.isTrue(false);
-		UnitTest.isTrue(true);
+		PrecedenceConstraint contrainte = new PrecedenceConstraint (sport, ip);
+		PrecedenceConstraintWithGap contrainteWithGap = new PrecedenceConstraintWithGap(options, ip, 60);
+		PrecedenceConstraintWithGap contrainteWithGap2 = new PrecedenceConstraintWithGap(options, ip, 30);
+		MeetConstraint meetConstraint = new MeetConstraint(sport, ip);
 
 		UnitTest.setTestLabel("PrecedenceConstraint");
-		UnitTest.isTrue(false);
-		UnitTest.isTrue(true);
-		UnitTest.isFalse(true);
+		UnitTest.isFalse(contrainte.isSatisfied(neufHeure, dixHeure));
+		UnitTest.isTrue(contrainte.isSatisfied(neufHeure, onzeHeure));
+		UnitTest.isTrue(contrainte.isSatisfied(neufHeure, douzeHeure));
+
+		UnitTest.setTestLabel("PrecedenceConstraintWithGap");
+		UnitTest.isFalse(contrainteWithGap.isSatisfied(neufHeure, dixHeure)); //2nde activité pendant la 1ère
+		UnitTest.isFalse(contrainteWithGap.isSatisfied(neufHeure, onzeHeure)); //2nde activité pendant la pause
+		UnitTest.isTrue(contrainteWithGap2.isSatisfied(neufHeure, onzeHeure)); //2nde activité pile poil après la pause
+		UnitTest.isTrue(contrainteWithGap.isSatisfied(neufHeure, douzeHeure)); //2nde activité après la pause
+
+		UnitTest.setTestLabel("MeetConstraint");
+		UnitTest.isFalse(meetConstraint.isSatisfied(neufHeure, dixHeure)); //2nde activité trop tôt
+		UnitTest.isTrue(meetConstraint.isSatisfied(neufHeure, onzeHeure)); //2nde activité en même temps
+		UnitTest.isFalse(meetConstraint.isSatisfied(neufHeure, douzeHeure)); //2nde activité trop tard
+		UnitTest.summary();
 	}
 
 }
