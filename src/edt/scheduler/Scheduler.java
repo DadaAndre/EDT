ea8492ae718371897bbd36f9ff5a435a0bf116dc
MartinJ <martinj50220@gmail.com>
2019-11-13 09:30:12 +0100

    refactor : Deplacement Javadoc

--- src/edt/scheduler/Scheduler.java
+++ src/edt/scheduler/Scheduler.java
@@ -11,17 +11,17 @@ import java.util.Map;
 /**
 * Regarde si un plan peut être créer en prenant en compte plusieurs contraintes
 */
-
 public class Scheduler {
 
-	private HashMap<Activity, Integer> initNbPreds(List<PrecedenceConstraint> contraintes) {
-		/**
-		* Créer une liste de contrainte afin de compter le nombre de predecesseurs de chaque activités.
-		*
-		* @param contraintes liste de PrecedenceConstraint créer à partir d'activité definie
-		* @return nbrPredecesseurs retourne une HashMap comprenant une activité et le nombre de ses prédécesseurs
+	
+	/**
+	* Créer une liste de contrainte afin de compter le nombre de predecesseurs de chaque activités.
+	*
+	* @param contraintes liste de PrecedenceConstraint créer à partir d'activité definie
+	* @return nbrPredecesseurs retourne une HashMap comprenant une activité et le nombre de ses prédécesseurs
 
-		*/
+	*/
+	private HashMap<Activity, Integer> initNbPreds(List<PrecedenceConstraint> contraintes) {
 		HashMap<Activity, Integer> nbrPredecesseurs = new HashMap<>();
 
 		for(PrecedenceConstraint precedConstraint : contraintes) {
@@ -40,17 +40,17 @@ public class Scheduler {
 		return nbrPredecesseurs;
 	}
 
-	//ajoute l'activité dans l'edt et réduit de -1 ses prédécesseurs
+
+	/**
+	* ajoute les activités un par une dans l'emploi du temps en fonction de son nombre de prédecesseurs.
+	*
+	* @param act activité  à ajouter dans l'emploi du temps dont le nombre de prédécesseur est à zéro
+	* @param heure nombre de minutes écoulé depuis la toute premiere activité ajouté dans l'emploi du temps
+	* @param contraintes liste de contrainte
+	* @param edt emploi du temps où les activités sont ajouté
+	* @param nbrPredecesseurs HashMap dont les activités, non encore implenté dans l'edt, sont enregistré avec le nombre de leurs predecesseurs (valeur > 0: pas encore ajouter dans l'edt, valeur = 0: activité qui est en train d'être rajouter dans l'edt)
+	*/
 	private void scheduleActivity(Activity act, int heure, List<PrecedenceConstraint> contraintes, HashMap<Activity, Integer> edt, HashMap<Activity, Integer> nbrPredecesseurs) {
-		/**
-		* ajoute les activités un par une dans l'emploi du temps en fonction de son nombre de prédecesseurs.
-		*
-		* @param act activité  à ajouter dans l'emploi du temps dont le nombre de prédécesseur est à zéro
-		* @param heure nombre de minutes écoulé depuis la toute premiere activité ajouté dans l'emploi du temps
-		* @param contraintes liste de contrainte
-		* @param edt emploi du temps où les activités sont ajouté
-		* @param nbrPredecesseurs HashMap dont les activités, non encore implenté dans l'edt, sont enregistré avec le nombre de leurs predecesseurs (valeur > 0: pas encore ajouter dans l'edt, valeur = 0: activité qui est en train d'être rajouter dans l'edt)
-		*/
 
 		edt.put(act, heure);
 
@@ -65,12 +65,13 @@ public class Scheduler {
 
 	}
 
+
+	/**
+	* méthode qui construit l'emploi du temps en fonction des contraintes
+	* @param contraintes liste de de contraintes
+	* @return retourne l'emploi du temps complet
+	*/
 	public HashMap<Activity, Integer> computeSchedule(List<PrecedenceConstraint> contraintes) {
-		/**
-		* méthode qui construit l'emploi du temps en fonction des contraintes
-		* @param contraintes liste de de contraintes
-		* @return retourne l'emploi du temps complet
-		*/
 		HashMap<Activity, Integer> nbrPredecesseurs = initNbPreds(contraintes);
 		HashMap<Activity, Integer> edt = new HashMap<Activity, Integer>();
 		int heure = 0;
