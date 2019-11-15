bf9d9f218f1e3446cb21c7e3da2b483157f77d72
Maxime.T <maxim50690@hotmail.com>
2019-11-15 14:48:59 +0100

    ajout de la javadoc

--- src/edt/constraints/MaxSpanConstraint.java
+++ src/edt/constraints/MaxSpanConstraint.java
@@ -6,18 +6,31 @@ import java.util.ArrayList;
 import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.List;
-
+/**
+ * 
+ */
 public class MaxSpanConstraint implements Constraint  {
-	
+	/**
+	 * Intervalle  de dure maximum pour faire les activités 
+	 */
 	private int dureeMax;
+	
+	/**
+	 * La liste des activités
+	 */
 	private ArrayList<Activity> liste;
+	
 	
 	public MaxSpanConstraint(int dureeMax){ 
 		this.dureeMax=dureeMax;
 		this.liste = new ArrayList<>();
 	}
 
-	 
+	/**
+	 * fonction qui regarde si l'ensemble des activités rentre dans l'intervalle de temps
+	 * @param edt l'emploi du temps
+	 * @return false si l'ensemble des activités prend plus de temps que l'intervalle de durée, sinon true 
+	 */
 	public boolean isSatisfiedBySchedule(HashMap<Activity, GregorianCalendar> edt) {
 		Activity plusTot = null;
 		Activity plusTard = null;
@@ -51,10 +64,15 @@ public class MaxSpanConstraint implements Constraint  {
 		
 		return (finPlusTard.getTime().getTime() - debutPlusTot.getTime().getTime())/(1000*60) <= this.dureeMax;
 	}
-
+	/**
+ 	 * ajoute une activite a la liste
+ 	 */
 	public void add(Activity a) {
 		this.liste.add(a);
 	}
+	/**
+     * permet de choisir l'intervalle max pour faire les activités
+     */
 	
 	public void setDureeMax(int dureeMax) {
 		this.dureeMax = dureeMax;
