6b70367be64d6946959c5dc1c31c1837d49f3d4c
MartinJ <martinj50220@gmail.com>
2019-11-17 21:50:56 +0100

    refactor : Remise en forme de Scheduler

--- src/edt/scheduler/Scheduler.java
+++ src/edt/scheduler/Scheduler.java
@@ -13,26 +13,27 @@ import java.util.Map;
 */
 public class Scheduler {
 
-
 	/**
 	* Compte le nombre de prédécesseurs de chaque activité selon les contraintes fournies en paramètre
 	*
 	* @param contraintes Liste des contraintes de précédence
 	* @return Retourne une HashMap comprenant une activité et le nombre de prédécesseurs qu'elle a
-
 	*/
 	private HashMap<Activity, Integer> initNbPreds(List<PrecedenceConstraint> contraintes) {
 		HashMap<Activity, Integer> nbrPredecesseurs = new HashMap<>();
 
 		for(PrecedenceConstraint precedConstraint : contraintes) {
-			if (! nbrPredecesseurs.containsKey(precedConstraint.getFirstActivity())) {
-				nbrPredecesseurs.put(precedConstraint.getFirstActivity(),0);
+			// La première activité n'est pas dans la map, on l'ajoute
+			if(!nbrPredecesseurs.containsKey(precedConstraint.getFirstActivity())) {
+				nbrPredecesseurs.put(precedConstraint.getFirstActivity(), 0);
 			}
 
-			if (nbrPredecesseurs.containsKey(precedConstraint.getSecondActivity())) {
+			// La deuxième activité est déjà dans la liste, on augmente d'un son nombre de prédécesseurs
+			if(nbrPredecesseurs.containsKey(precedConstraint.getSecondActivity())) {
 				Integer valeur = nbrPredecesseurs.get(precedConstraint.getSecondActivity());
 				nbrPredecesseurs.put(precedConstraint.getSecondActivity(), valeur+1);
 			} else {
+				// Sinon si l'activité n'y st pas, on l'ajoute en spécifiant qu'elle à 1 prédécesseur
 				nbrPredecesseurs.put(precedConstraint.getSecondActivity(), 1);
 			}
 		}
@@ -51,18 +52,19 @@ public class Scheduler {
 	* @param nbrPredecesseurs HashMap comprenant une activité et le nombre de prédécesseurs qu'elle a
 	*/
 	private void scheduleActivity(Activity act, int heure, List<PrecedenceConstraint> contraintes, HashMap<Activity, Integer> edt, HashMap<Activity, Integer> nbrPredecesseurs) {
-
+		// On ajoute l'activité sur lemploi du temps
 		edt.put(act, heure);
 
-		for (PrecedenceConstraint precedConstraint : contraintes) {
-			if (precedConstraint.getFirstActivity() == act) {
+		for(PrecedenceConstraint precedConstraint : contraintes) {
+			// Si l'activité plannifé est prédécesseur dans la contrainte actuelle, on enlève 1 au nombre de prédecesseurs
+			if(precedConstraint.getFirstActivity() == act) {
 				Integer valeur = nbrPredecesseurs.get(precedConstraint.getSecondActivity());
 				nbrPredecesseurs.put(precedConstraint.getSecondActivity(), valeur-1);
 			}
 		}
 
+		// On enlève l'activité plannifié pour ne pas la replannifier à l'infini
 		nbrPredecesseurs.remove(act);
-
 	}
 
 
@@ -79,21 +81,21 @@ public class Scheduler {
 		while(nbrPredecesseurs.size() > 0) {
 			Activity actZero = null;
 
-			for (Map.Entry<Activity, Integer> entry : nbrPredecesseurs.entrySet()) {
-				// si l'activité a zéro prédécesseur
-				if (entry.getValue() == 0) {
-					// met l'activité dans une variable (= on la sélectionne)
+			for(Map.Entry<Activity, Integer> entry : nbrPredecesseurs.entrySet()) {
+				// Si l'activité a zéro prédécesseur on la sélectionne
+				if(entry.getValue() == 0) {
 					actZero = entry.getKey();
-					// stop la boucle si il trouve une activité à 0 prédécesseur
+					// Stop la boucle si il trouve une activité à 0 prédécesseur
 					break;
 				}
 			}
 
 			// Si on a aucune activité avec 0 prédécesseur, on arrête car aucun plan n'est possible
-			if (actZero == null) {
+			if(actZero == null) {
 				return null;
 			}
 
+			// Sinon on la plannifie
 			scheduleActivity(actZero, heure, contraintes, edt, nbrPredecesseurs);
 			heure += actZero.getDuree();
 		}
