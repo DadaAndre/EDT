3b5958c5d7fdd39edb2d87bc79f87cdccb92db53
MartinJ <martinj50220@gmail.com>
2019-11-17 19:47:34 +0100

    refactor : PrecedenceConstraintWithGap

--- src/edt/constraints/PrecedenceConstraintWithGap.java
+++ src/edt/constraints/PrecedenceConstraintWithGap.java
@@ -1,8 +1,9 @@
 package edt.constraints;
 
 import edt.activity.Activity;
-import java.util.HashMap;
+
 import java.util.GregorianCalendar;
+import java.util.HashMap;
 
 /**
 * Contrainte binaire satisfaite lorsque qu'il s'écoule au minimum un certain délai entre la fin de la première activité et le début de la deuxième
@@ -20,8 +21,8 @@ public class PrecedenceConstraintWithGap extends PrecedenceConstraint {
 	* @param second La deuxième activité
 	* @param gap Le temps minimum entre la fin de la première activité et le début de la deuxième (en minutes)
 	*/
-	public PrecedenceConstraintWithGap(Activity activite1, Activity activite2, int gap) {
-		super(activite1, activite2);
+	public PrecedenceConstraintWithGap(Activity first, Activity second, int gap) {
+		super(first, second);
 
 		this.gap = gap;
 	}
@@ -29,13 +30,13 @@ public class PrecedenceConstraintWithGap extends PrecedenceConstraint {
 
 	@Override
 	public boolean isSatisfied(GregorianCalendar dateDebutAct1, GregorianCalendar dateDebutAct2) {
-		GregorianCalendar dateFinAct1 = new GregorianCalendar(); //création d'un nouveau calendrier.
-		dateFinAct1.setTime(dateDebutAct1.getTime()); // mettre ce nouveau calendrier à la même date que la première activité.
-		dateFinAct1.add(GregorianCalendar.MINUTE, this.getFirstActivity().getDuree()+this.gap); //on ajoute à l'heure de la première activité, sa durée et la pause.
+		// On récupère la fin de la première activité + le gap
+		GregorianCalendar dateFinAct1 = new GregorianCalendar();
+		dateFinAct1.setTime(dateDebutAct1.getTime());
+		dateFinAct1.add(GregorianCalendar.MINUTE, this.getFirstActivity().getDuree() + this.gap);
 
-		/* on compare les deux activités: si la deuxième activité
-		* commence après la fin de la première activité (et le gap), alors le compareTo est un nombre inferieur à "0".
-		*/
+
+		// On vérifie si la fin de l'activité et du gap se passe avant le début de la seconde activité
 		return dateFinAct1.compareTo(dateDebutAct2) <= 0;
 	}
 
