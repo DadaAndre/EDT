60607247a4ac5d7a8208342244dfcfb25aa74750
MartinJ <martinj50220@gmail.com>
2019-11-17 16:49:44 +0100

    doc : Commentaire dans Main

--- src/edt/Main.java
+++ src/edt/Main.java
@@ -17,6 +17,7 @@ import java.util.Map;
 
 public class Main {
 
+	// Constantes pour les erreurs
 	private static final int ERROR_INVALID_COMMAND_USAGE = 1;
 	private static final int ERROR_FILE_NOT_FOUND = 2;
 	private static final int ERROR_IO_ERROR = 3;
@@ -26,6 +27,7 @@ public class Main {
 			showCommandUsage();
 		}
 
+		// On lit les activités
 		RandomScheduler randomScheduler = new RandomScheduler();
 		Reader reader = null;
 		try {
@@ -38,6 +40,7 @@ public class Main {
 			System.exit(ERROR_IO_ERROR);
 		}
 
+		// On lit les contraintes
 		Collection<Constraint> constraints = null;
 		try {
 			constraints = reader.readConstraints();
@@ -49,14 +52,17 @@ public class Main {
 			System.exit(ERROR_IO_ERROR);
 		}
 
+		// On ajoute les activités à randomScheduler
 		for(Activity a : reader.getActivities()) {
 			randomScheduler.addActivity(a);
 		}
 
+		// On ajoute les contraintes à randomScheduler
 		for(Constraint c : constraints) {
 			randomScheduler.addConstraint(c);
 		}
 
+		// On génère 50 edt par défaut, on vérifie si le 3ème paramètre est présent
 		int nbGenerationEdt = 50;
 		if(args.length == 3) {
 			try {
@@ -66,14 +72,17 @@ public class Main {
 			}
 		}
 
+		// On récupère le meilleur emploi du temps
 		HashMap<Activity, GregorianCalendar> edt = randomScheduler.edtWithMostSatisfiedConstraint(nbGenerationEdt);
 		DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 
+		// On affiche l'edt
 		for(Map.Entry<Activity, GregorianCalendar> entry : edt.entrySet()) {
 			System.out.println(entry.getKey() + " : " + dateFormat.format(entry.getValue().getTime()));
 		}
 	}
 
+	// On affiche la commande à utiliser
 	public static void showCommandUsage() {
 		System.out.println("Usage : ");
 		System.out.println("java ... <fichierActivités> <fichierContraintes> [nombreGenerationEdt]");
