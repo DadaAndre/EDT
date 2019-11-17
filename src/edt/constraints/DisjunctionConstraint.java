7fc384dacb2cb797e4e7513a7f1ceddfa84e374c
MartinJ <martinj50220@gmail.com>
2019-11-17 17:49:57 +0100

    refactor : Remise en forme DisjunctionConstraint

--- src/edt/constraints/DisjunctionConstraint.java
+++ src/edt/constraints/DisjunctionConstraint.java
@@ -7,39 +7,32 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
-* Prend en compte deux contraintes et regarde si au moins une est satisfaite
+* Prend en compte deux contraintes et vérifie si au moins l'une des deux est satisfaite
 */
-
 public class DisjunctionConstraint implements Constraint {
 
 	/**
 	* Première contrainte
 	*/
-	Constraint c1;
+	private Constraint c1;
 
 	/**
 	* Deuxième contrainte
 	*/
-	Constraint c2;
+	private Constraint c2;
 
 
 	/**
 	* @param c1 Première contrainte
 	* @param c2 Deuxième contrainte
 	*/
-    public DisjunctionConstraint(Constraint c1, Constraint c2) {
-        this.c1 = c1;
-        this.c2 = c2;
-    }
+	public DisjunctionConstraint(Constraint c1, Constraint c2) {
+		this.c1 = c1;
+		this.c2 = c2;
+	}
 
 
-	/**
-	* Prend un emploi du temps et regarde si au moins une des contrainte est satisfaite
-	*
-	* @param edt emploi du temps
-	* @return Retourne false si aucune des contraintes n'est satisfaites, true si au moins une des contraintes est satisfaite
-	*/
-    public boolean isSatisfiedBySchedule(HashMap<Activity,GregorianCalendar> edt) {
-		return c1.isSatisfiedBySchedule(edt) || c2.isSatisfiedBySchedule(edt);
+	public boolean isSatisfiedBySchedule(HashMap<Activity, GregorianCalendar> edt) {
+		return this.c1.isSatisfiedBySchedule(edt) || this.c2.isSatisfiedBySchedule(edt);
 	}
 }
